2c9e2bbdabcee61cefcb32b1b770a241eb79ffa2
majakk <mattias.jacobsson@gmail.com>
2014-08-12 00:28:33 +0200

    added new pic

--- core/src/studio/coldstream/citylwp/MainGame.java
+++ core/src/studio/coldstream/citylwp/MainGame.java
@@ -13,15 +13,16 @@ public class MainGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("gfx/new3_big.png");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0.5f, 0.6f, 0.7f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
+
 	}
 }
